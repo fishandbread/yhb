604a0783c7953d66da2ac08376db4416a467d9a9
lxr130807 <75208812+lxr130807@users.noreply.github.com>
2022-01-01 19:52:05 +0800

    Add files via upload

---
+++ yhb/tongji.js
@@ -0,0 +1,29 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?cf7a043afd8d6212fda716a110653110";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
+(function(){
+var src = "https://s.ssl.qhres2.com/ssl/ab77b6ea7f3fbf79.js";
+document.write('<script src="' + src + '" id="sozz"><\/script>');
+})();
+	
+var div = document.createElement("div");
+div.innerHTML = '<br><br>';
+document.body.appendChild(div);
+
+var script = document.createElement("script");
+script.type = "text/javascript";
+script.src = "//1.donglih.com/source/f_a_z/resource/z/gjzww.js";
+document.body.appendChild(script);
+
+/*var script = document.createElement("script");
+script.type = "text/javascript";
+script.src = "//1.donglih.com/production/p/kj_jqsl_hg.js";
+document.body.appendChild(script);*/
+	
+var div = document.createElement("div");
+div.innerHTML = '<br><br><br>';
+document.body.appendChild(div);
